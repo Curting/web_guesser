6c5d044b2535ba623da5a5db45a32ff1833a440a
Oliver <oc@hejverden.dk>
2017-11-29 12:48:20 +0100

    I4: Change background-color depending on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,21 +2,23 @@
 require "sinatra"
 require "sinatra/reloader"
 
-SECRET_NUMBER = rand(101)
+set :number, rand(101)
 
 def check_guess(guess)
-  case guess - SECRET_NUMBER
-    when 10..100 then "Too damn high!"
-    when 1...10 then "A little too high..."
-    when 0 then "YAY! You guessed it."
-    when -10...0 then "A little too low..."
-    when -100..-11 then "Waaaay too low!"
+  case guess - settings.number
+    when 10..100 then ["Too damn high!", "red"]
+    when 1...10 then ["A little too high...", "salmon"]
+    when 0 then ["YAY! You guessed it.", "green"]
+    when -10...0 then ["A little too low...", "salmon"]
+    when -100..-11 then ["Waaaay too low!", "red"]
   end
 end
 
 
 get '/' do
   guess = params["guess"].to_i
-  message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  evaluate = check_guess(guess)
+  message = evaluate[0]
+  background_color = evaluate[1]
+  erb :index, :locals => {:number => settings.number, :message => message, :background_color => background_color}
 end
